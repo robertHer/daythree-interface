31f770eaa04c3bc66f07e025d0ca8d7e3322bcb5
unknown <totolero769@gmail.com>
2018-06-01 11:27:13 -0700

    practice for interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+interface Person {
+  firstName:string;
+  lastName:string;
+
+  sayHi(): string;
+   
+}
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit(){
+    this.practice2Test();
+  }
+
+
+
+
+practice2Test(){
+
+  const customer:Person = { 
+    firstName:"bob",
+    lastName:"smith",
+    sayHi: () =>{return "Hi there"} 
+ } 
+ console.log(customer.firstName, customer.lastName , customer.sayHi()) 
+ 
+ 
+ const employee:Person = { 
+    firstName:"jamie",
+    lastName:"smith", 
+    sayHi: ()=>{return "Hello!!!"} 
+ } 
+   
+ console.log(employee.firstName, employee.lastName, employee.sayHi()) 
+
+}
+
 }
